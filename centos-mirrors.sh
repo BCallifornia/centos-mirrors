7a4e8c8f4ca347691450fc00ec74bea80f2e3e06
Brandon Callifornia <BCallifornia@users.noreply.github.com>
2019-08-27 22:04:51 +0200

    U centos mirror list

--- centos-mirrors.sh
+++ centos-mirrors.sh
@@ -2,3 +2,6 @@
 
 
 curl https://www.centos.org/download/full-mirrorlist.csv | sed 's/^.*"http:/http:/' | sed 's/".*$//' | grep ^http >./centos_mirrors
+git add centos_mirrors
+git commit -m "U centos mirror list"
+git push
