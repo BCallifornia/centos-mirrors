7162de6e9af22efeae28e94cd30029960c25fcde
Brandon Callifornia <BCallifornia@users.noreply.github.com>
2019-09-04 15:07:54 +0200

    U centos-mirrors.sh inserted timestamp into commit message

--- centos-mirrors.sh
+++ centos-mirrors.sh
@@ -3,5 +3,5 @@
 
 curl https://www.centos.org/download/full-mirrorlist.csv | sed 's/^.*"http:/http:/' | sed 's/".*$//' | grep ^http >./centos_mirrors
 git add centos_mirrors
-git commit -m "U centos mirror list"
+git commit -m "U centos mirror list $(date)"
 git push
