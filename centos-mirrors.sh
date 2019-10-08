0fd486183fb5956bb2ca6e7dac2dca67d4089551
Brandon Callifornia <BCallifornia@users.noreply.github.com>
2019-10-08 12:19:28 +0200

    U centos-mirrors.sh A cd into path or else exit

--- centos-mirrors.sh
+++ centos-mirrors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-
+cd /home/bcallifornia/workspace/gh_centos_mirrors/ || exit
 curl https://www.centos.org/download/full-mirrorlist.csv | sed 's/^.*"http:/http:/' | sed 's/".*$//' | grep ^http >./centos_mirrors
 git add centos_mirrors
 git commit -m "U centos mirror list $(date)"
