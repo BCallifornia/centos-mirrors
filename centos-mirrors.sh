491451c872b94e2cd1b62cc05d705d8c93b72dfd
Brandon Callifornia <BCallifornia@users.noreply.github.com>
2020-03-02 18:25:51 +0100

    U centos-mirrors.sh U path
    Updated the path for the new workspace layout.

--- centos-mirrors.sh
+++ centos-mirrors.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd "$HOME/workspace/gh_centos_mirrors/" || exit
+cd "$HOME/workspace/github/bcallifornia_centos-mirrors/" || exit
 curl https://www.centos.org/download/full-mirrorlist.csv | sed 's/^.*"http:/http:/' | sed 's/".*$//' | grep ^http >./centos_mirrors
 git add centos_mirrors
 git commit -m "U centos mirror list $(date)"
